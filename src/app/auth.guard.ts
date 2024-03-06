808634c961befb774e1092d8848d8bddd2029119
khadidiatou6c <khadidiatoucisse2018@outlook.fr>
2024-03-06 08:30:48 +0000

    refac all

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -6,29 +6,29 @@ export const authGuard: CanActivateFn = (route, state) => {
   const router = new Router();
 const  userOnline = JSON.parse(localStorage.getItem('userOnline') || "") 
  /**gard pour utilisateur simple */
-  // if (userOnline.role_id===2   ) {
-  //   return true;
-  // }else{
-  //   router.navigate(['/authentification'])
-  //   return false;
-  // }
-
-  if (localStorage.getItem('access_token')==null || localStorage.getItem('access_token')==undefined) {
-    Swal.fire({
-      icon:'error',
-      text:'Connectez-vous si vous voulez acceder à cet espace',
-      title:'Oops',
-      confirmButtonColor: "#2ECC71",
-    }
-      
-    )
-    router.navigate(['/authentification']);
-    return false;
-
+  if (userOnline.role_id===2   ) {
+    return true;
   }else{
+    router.navigate(['/authentification'])
+    return false;
+  }
 
-    return true;
-  } 
+//   if (localStorage.getItem('access_token')==null || localStorage.getItem('access_token')==undefined) {
+//     Swal.fire({
+//       icon:'error',
+//       text:'Connectez-vous si vous voulez acceder à cet espace',
+//       title:'Oops',
+//       confirmButtonColor: "#2ECC71",
+//     }
+      
+//     )
+//     router.navigate(['/authentification']);
+//     return false;
+
+//   }else{
+
+//     return true;
+//   } 
 // if (localStorage.getItem('access_token')==null || localStorage.getItem('access_token')==undefined) {
 //     Swal.fire({
 //       icon:'error',
